08f8a9191068e78ee7f6783b8d5ab96b8edc6d68
MauricioZapata00 <maurozac123@gmail.com>
2022-04-02 15:38:55 -0500

    Now the registration of an user is complete

--- src/containers/Login.jsx
+++ src/containers/Login.jsx
@@ -2,38 +2,83 @@ import React, { useState } from "react";
 import '../App.css';
 import Google from '../assets/img/googleLogo.jpg';
 import GitHub from '../assets/img/GitHubLogo.png'
-import { auth } from "../firebase/credentials";
-import { createUserWithEmailAndPassword } from "firebase/auth";
+import { auth, db } from "../firebase/credentials";
+import { getFirestore, doc, collection, setDoc } from "firebase/firestore";
+import { createUserWithEmailAndPassword, signInWithEmailAndPassword } from "firebase/auth";
 
 const Login = () => {
     const authentication = auth;
-    const [user, setUser] = useState(null)
+    const firestore = db;
+    const [registered, setRegistered] = useState(false);
 
-    const registerUser = async(email, password) =>{
-        console.log("Presionaste Registro")
-        const infoUser = await createUserWithEmailAndPassword(authentication,email, password)
-        console.log(infoUser);
+    const registrarUsuario = async(email, password) => {
+        const infoUser = await createUserWithEmailAndPassword(authentication,
+            email,
+            password).catch((error) =>{
+                console.log(error);
+            }).then((firestoreUser) => {
+                return firestoreUser
+        })
+        console.log(infoUser.user.uid);
+        const docuRef = doc(firestore, `raulsHardwareStore/${infoUser.user.uid}`);
+        setDoc(docuRef, { correo: email, uid: infoUser.user.uid });
+
     }
+
+    const accessUser = async (e) => {
+
+        e.preventDefault();
+        const email = e.target.elements.user.value;
+        const password = e.target.elements.password.value;
+
+        console.log("Presionaste Registro")
+        console.log(email);
+        console.log(password);
+        //console.log(registered);
+        switch (registered) {
+            case true:
+                signInWithEmailAndPassword(authentication, email, password)
+                break;
+            case false:
+                registrarUsuario(email, password);
+                break;
+        }
+        /*
+        const infoUser = await createUserWithEmailAndPassword(authentication,
+            email, 
+            password
+            ).catch((error) => {
+                console.log(error);
+            })
+        console.log(infoUser);
+        */
+    }
+    
     return (
         <div>
-            <div className="center-block">
-                <div>
-                    <input type="text" className="form-control" placeholder="Usuario" aria-label="Username" />
+            <form className="center-block" onSubmit={accessUser}>
+                <div className="mb-3">
+                    <label htmlFor="exampleInputEmail1" className="form-label">Correo</label>
+                    <input type="email" className="form-control" id="user" aria-describedby="emailHelp" />
+                    <div id="emailHelp" className="form-text">Ingresa tu correo electrónico para registrarte</div>
                 </div>
-            </div>
-            <div className="center-block">
-                <div>
-                    <input type="text" className="form-control" placeholder="Contraseña" aria-label="Server" />
+                <div className="mb-3">
+                    <label htmlFor="exampleInputPassword1" className="form-label" >Contraseña</label>
+                    <input type="password" className="form-control" id="password" placeholder="Contraseña" required minLength="6" />
                 </div>
-            </div>
+                <div className="mb-3 form-check">
+                    <input type="checkbox" className="form-check-input" id="registered" onChange={e => setRegistered(e.target.checked)}/>
+                    <label className="form-check-label" htmlFor="exampleCheck1">Ya tengo cuenta</label>
+                </div>
+                <button type="submit" className="btn btn-secondary">
+                    Registrar - Acceder
+                </button>
+
+            </form>
+
             
             <div className="center-block">
-                <button type="button" className="btn btn-secondary" onClick={() =>registerUser()}>
-                    Registro
-                </button>
-            </div>
-            <div className="center-block">
-                <button type="button" className="btn btn-success">
+                <button type="submit" className="btn btn-success">
                     <div className="boton-login">
                         <img src={Google} className="icon" alt="Google" />
                         <div className="login">
@@ -43,9 +88,9 @@ const Login = () => {
                 </button>
             </div>
             <div className="center-block">
-                <button type="button" className="btn btn-dark">
+                <button type="submit" className="btn btn-dark">
                     <div className="boton-login">
-                        <img src={GitHub} className="icon" alt="Google" />
+                        <img src={GitHub} className="icon" alt="GitHub" />
                         <div className="login">
                             Login con GitHub
                         </div>
